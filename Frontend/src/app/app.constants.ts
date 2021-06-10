c04867b4f689eec52ed70ff0368f5d662b5491fe
KristinaNikolic99 <krisnikolic992@gmail.com>
2021-06-10 12:27:52 +0200

    Heroku

--- Frontend/src/app/app.constants.ts
+++ Frontend/src/app/app.constants.ts
@@ -1,5 +1,11 @@
-export const FAKULTET_URL= 'http://localhost:8083/fakultet';
-export const STATUS_URL= 'http://localhost:8083/status';
-export const STUDENT_URL= 'http://localhost:8083/student';
-export const DEPARTMAN_URL = 'http://localhost:8083/departman';
-export const DEPARTMAN_ZA_FAKULTET_URL = 'http://localhost:8083/departmanId';
+//export const FAKULTET_URL= 'http://localhost:8083/fakultet';
+//export const STATUS_URL= 'http://localhost:8083/status';
+//export const STUDENT_URL= 'http://localhost:8083/student';
+//export const DEPARTMAN_URL = 'http://localhost:8083/departman';
+//export const DEPARTMAN_ZA_FAKULTET_URL = 'http://localhost:8083/departmanId';
+
+export const FAKULTET_URL= 'https://backendrva1.herokuapp.com/fakultet';
+export const STATUS_URL= 'https://backendrva1.herokuapp.com/status';
+export const STUDENT_URL= 'https://backendrva1.herokuapp.com/student';
+export const DEPARTMAN_URL = 'https://backendrva1.herokuapp.com/departman';
+export const DEPARTMAN_ZA_FAKULTET_URL = 'https://backendrva1.herokuapp.com/departmanId';
